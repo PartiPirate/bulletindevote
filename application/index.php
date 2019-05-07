2ad7de7e77387fbaf2d018fc151eac72f8ab888a
farlistener <contact@levieuxcedric.com>
2019-05-07 22:23:54 +0000

    Upgrade bulletin

---
+++ application/index.php
@@ -0,0 +1,59 @@
+<!--
+	Copyright 2019 Cédric Levieux, Parti Pirate
+
+	This file is part of BulletinDeVote.
+
+    BulletinDeVote is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    BulletinDeVote is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with BulletinDeVote.  If age, see <http://www.gnu.org/licenses/>.
+-->
+<html>
+<head>
+<title></title>
+    <meta http-equiv="refresh" content="0; URL=index.html">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta name="description" content="">
+	<meta name="author" content="Parti Pirate">
+
+	<title>Mon Bulletin De Vote</title>
+
+	<!-- Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://bulletindevote.partipirate.org/" />
+	<meta property="og:title" content="Parti Pirate - Mon Bulletin De Vote" />
+	<meta property="og:description" content="Imprimer votre bulletin de vote pour le Parti Pirate" />
+	<meta property="og:image" content="https://bulletindevote.partipirate.org/assets/img/logo_voile_fond.png" />
+	<meta property="og:locale" content="fr_FR" />
+	<meta property="og:locale:alternate" content="en_US" />
+	<meta property="fb:page_id" content="partipiratefr" />
+
+	<!-- Google +1 -->
+	<meta itemprop="name" content="Parti Pirate - Mon Bulletin De Vote" />
+	<meta itemprop="description" content="Imprimer votre bulletin de vote pour le Parti Pirate" />
+	<meta itemprop="image" content="https://bulletindevote.partipirate.org/assets/img/logo_voile_fond.png" />
+	<meta itemprop="author" content="farlistener" />
+
+	<!-- Twitter -->
+	<meta name="twitter:site" content="@partipirate" />
+	<meta name="twitter:creator" content="@farlistener" />
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:url" content="https://bulletindevote.partipirate.org/" />
+	<meta name="twitter:title" content="Parti Pirate - Europ&eacute;ennes 2019" />
+	<meta name="twitter:description" content="Imprimer votre bulletin de vote pour le Parti Pirate" />
+	<meta name="twitter:image" content="https://bulletindevote.partipirate.org/assets/img/logo_voile_fond.png" />
+	<meta name="twitter:image:alt" content="Logo du Parti Pirate" />
+
+</head>
+<body>
+</body>
+</html>
